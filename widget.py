2553face7173b09178127dd1b019d89bb7fee948
Anirban Dey <code@soufrabi.com>
2023-01-23 17:28:13 +0530

    Search bar caption

--- widget.py
+++ widget.py
@@ -1,5 +1,5 @@
 from PySide6 import QtGui
-from PySide6.QtWidgets import QWidget, QTextEdit, QHBoxLayout, QVBoxLayout, QPushButton, QMessageBox, QSizePolicy, QTextBrowser, QLineEdit
+from PySide6.QtWidgets import QWidget, QTextEdit, QHBoxLayout, QVBoxLayout, QPushButton, QMessageBox, QSizePolicy, QTextBrowser, QLineEdit, QLabel
 from PySide6.QtGui import QClipboard
 from apicontroller import APIController
 from storage import Storage
@@ -18,7 +18,7 @@ class Widget(QWidget):
 
 
 
-        self.setMinimumSize(1400, 700)
+        self.setMinimumSize(700, 500)
         self.input_text_edit = QTextEdit()
         self.input_text_edit.setFixedHeight(100)
         # self.input_text_edit.textChanged.connect(self.text_changed)
@@ -31,29 +31,12 @@ class Widget(QWidget):
         # connect the search bar to the text browser
         self.search_bar.textChanged.connect(self.history_text_browser.find)
         self.search_bar.returnPressed.connect(self.history_text_browser.find)
-        self.search_bar.editingFinished.connect(self.history_text_browser.find)
+        # self.search_bar.editingFinished.connect(self.history_text_browser.find)
 
         # Buttons
-        # copy_button = QPushButton("Copy")
-        # copy_button.clicked.connect(self.input_text_edit.copy)  # Connect to built in QTextEdit slot
-        #
-        # cut_button = QPushButton("Cut")
-        # cut_button.clicked.connect(self.input_text_edit.cut)
-        #
-        # paste_button = QPushButton("Paste")  # Custom paste not built-in
-        # paste_button.clicked.connect(self.paste_button_clicked)
-
-        # undo_button = QPushButton("Undo")
-        # undo_button.clicked.connect(self.input_text_edit.undo)
-        #
-        # redo_button = QPushButton("Redo")
-        # redo_button.clicked.connect(self.input_text_edit.redo)
 
         clear_input_button = QPushButton("Clear Input")
         clear_input_button.clicked.connect(self.input_text_edit.clear)
-
-
-
 
         save_button = QPushButton("Save")
         save_button.clicked.connect(self.save_button_clicked)
@@ -64,10 +47,6 @@ class Widget(QWidget):
         show_history_button = QPushButton("Show History")
         show_history_button.clicked.connect(self.show_history_button_clicked)
 
-        # read_button = QPushButton("Read")
-        # read_button.clicked.connect(self.read_button_clicked)
-
-
         copy_output_button = QPushButton("Copy Output")
         copy_output_button.clicked.connect(self.copy_output_button_clicked)
 
@@ -75,52 +54,41 @@ class Widget(QWidget):
         submit_button.clicked.connect(self.submit_button_clicked)
 
         history_next_button = QPushButton("Next")
-        history_next_button.clicked.connect(self.history_text_browser.forward)
+        # history_next_button.clicked.connect(self.history_text_browser.find(self.search_bar.text()))
+        # history_next_button.clicked.connect(self.history_text_browser.forward)
 
         history_previous_button = QPushButton("Previous")
-        history_previous_button.clicked.connect(self.history_text_browser.backward)
+        # history_previous_button.clicked.connect(self.history_text_browser.backward)
 
 
 
-        # connect the search bar to the search function
-        # self.search_bar.returnPressed.connect(self.search)
 
         # Layout
         h_layout1 = QHBoxLayout()
-        # h_layout1.addWidget(copy_button)
-        # h_layout1.addWidget(cut_button)
-        # h_layout1.addWidget(paste_button)
-        # h_layout1.addWidget(undo_button)
-        # h_layout1.addWidget(redo_button)
-
         h_layout1.addWidget(show_history_button)
-
         h_layout1.addWidget(clear_input_button)
         h_layout1.addWidget(clear_all_button)
-
-
         h_layout1.addWidget(history_previous_button)
         h_layout1.addWidget(history_next_button)
-
         h_layout1.addWidget(copy_output_button)
-
         h_layout1.addWidget(save_button)
         h_layout1.addWidget(submit_button)
-        # h_layout1.addWidget(read_button)
 
         v_layout_left = QVBoxLayout()
         v_layout_left.addWidget(self.input_text_edit)
         v_layout_left.addWidget(self.output_text_edit)
 
+        h_layout_search_bar = QHBoxLayout()
+        h_layout_search_bar.addWidget(QLabel("Search  :"))
+        h_layout_search_bar.addWidget(self.search_bar)
+
         v_layout_right = QVBoxLayout()
-        v_layout_right.addWidget(self.search_bar)
+        v_layout_right.addLayout(h_layout_search_bar)
         v_layout_right.addWidget(self.history_text_browser)
 
         h_layout2 = QHBoxLayout()
         h_layout2.addLayout(v_layout_left)
         h_layout2.addLayout(v_layout_right)
-        # h_layout2.addWidget(self.input_text_edit)
-        # h_layout2.addWidget(self.history_text_browser)
 
         v_layout = QVBoxLayout()
         v_layout.addLayout(h_layout1)
@@ -128,11 +96,6 @@ class Widget(QWidget):
 
         self.setLayout(v_layout)
 
-    def current_text_button_clicked(self):
-        print("Current Text : ", self.input_text_edit.toPlainText())
-
-    def paste_button_clicked(self):
-        self.input_text_edit.paste()
 
     def submit_button_clicked(self):
         print("Submit button clicked")
@@ -188,13 +151,8 @@ class Widget(QWidget):
         self.input_text_edit.clear()
         self.output_text_edit.clear()
 
-    # def search(self):
-    #     self.history_text_browser.setHtml(self.search_bar.text())
     def show_history_button_clicked(self):
         print("Show History Button clicked")
-        # message = QMessageBox()
-        # message.setMinimumSize(700, 300)
-        # message.setWindowTitle("Chat History")
 
         storage = Storage()
         history_list = storage.get_history()
@@ -203,28 +161,7 @@ class Widget(QWidget):
         history_string = ""
         for i in  range(len(history_list)):
             history_string += "Input : \n" + history_list[i]["input"]+"\n"
-            history_string += "Output : \n" + history_list[i]["output"]+"\n"
+            history_string += "Output : " + history_list[i]["output"]+"\n"
             history_string += "DateTIme : \n" + history_list[i]["datetime"]+"\n"
 
         self.history_text_browser.setPlainText(history_string)
-        # message.setStyleSheet("QLabel{min-width: 1100px;}")
-        # message.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        # message.setText(f"This is the chat history \n {history_string}")
-        # message.setInformativeText("Do you want to do something about it")
-
-        # message.setIcon(QMessageBox.Critical)
-        # message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        # message.setDefaultButton(QMessageBox.Ok)
-
-        # Show the message box
-        # ret = message.exec()
-        # if ret == QMessageBox.Ok:
-        #     print("User chose Ok")
-        # elif ret == QMessageBox.Cancel:
-        #     print("User chose Cancel")
-        # else:
-        #     print("User chose unknown button")
-
-    # def read_button_clicked(self):
-    #     self.storage.read()
-        # define the search function
