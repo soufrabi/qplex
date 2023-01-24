ee719569b72d23646c9e822892e57ee088d53023
Anirban Dey <code@soufrabi.com>
2023-01-25 00:33:53 +0530

    About/Help menu added

--- menu_bar_main.py
+++ menu_bar_main.py
@@ -2,8 +2,10 @@ from PySide6.QtWidgets import QMenuBar, QMessageBox
 from PySide6.QtGui import QAction
 from storagehistory import StorageHistory
 from edit_query_settings_ui import EditQuerySettingsDialog
+import os
+
 class MenuBarMain(QMenuBar):
-    def __init__(self,app,main_widget,status_bar):
+    def __init__(self, app, main_widget, status_bar):
         super().__init__()
 
         # Menubar and menus
@@ -19,31 +21,31 @@ class MenuBarMain(QMenuBar):
         edit_menu = self.addMenu("Edit")
         # Edit Copy Action
         edit_copy_action = QAction("Copy", self)
-        edit_copy_action.setStatusTip("Press this to See History")
+        edit_copy_action.setStatusTip("Copy selected text in Input Box to Clipboard")
         edit_copy_action.triggered.connect(main_widget.input_text_edit.copy)
         edit_menu.addAction(edit_copy_action)
 
         # Edit Cut Action
         edit_cut_action = QAction("Cut", self)
-        edit_cut_action.setStatusTip("Press this to See History")
+        edit_cut_action.setStatusTip("Cut selected text in Input Box to Clipboard")
         edit_cut_action.triggered.connect(main_widget.input_text_edit.cut)
         edit_menu.addAction(edit_cut_action)
 
         # Edit Paste Action
         edit_paste_action = QAction("Paste", self)
-        edit_paste_action.setStatusTip("Press this to See History")
+        edit_paste_action.setStatusTip("Paste clipboard content to Input Box")
         edit_paste_action.triggered.connect(main_widget.input_text_edit.paste)
         edit_menu.addAction(edit_paste_action)
 
         # Edit Undo Action
         edit_undo_action = QAction("Undo", self)
-        edit_undo_action.setStatusTip("Press this to See History")
+        edit_undo_action.setStatusTip("Undo changes in Input Box")
         edit_undo_action.triggered.connect(main_widget.input_text_edit.undo)
         edit_menu.addAction(edit_undo_action)
 
         # Edit Redo Action
         edit_redo_action = QAction("Redo", self)
-        edit_redo_action.setStatusTip("Press this to See History")
+        edit_redo_action.setStatusTip("Redo changes in Input Box")
         edit_redo_action.triggered.connect(main_widget.input_text_edit.redo)
         edit_menu.addAction(edit_redo_action)
 
@@ -52,13 +54,13 @@ class MenuBarMain(QMenuBar):
 
         # Show History Action
         show_history_action = QAction("Show History", self)
-        show_history_action.setStatusTip("Press this to See History")
+        show_history_action.setStatusTip("See Chat History")
         show_history_action.triggered.connect(self.show_history_action_clicked)
         history_menu.addAction(show_history_action)
 
         # Clear History Action
         clear_history_action = QAction("Clear History", self)
-        clear_history_action.setStatusTip("Press this to Clear History")
+        clear_history_action.setStatusTip("Clear Chat History")
         clear_history_action.triggered.connect(self.clear_history_action_clicked)
         history_menu.addAction(clear_history_action)
 
@@ -67,20 +69,134 @@ class MenuBarMain(QMenuBar):
 
         # Edit Settings Action
         edit_settings_action = QAction("Edit Settings", self)
-        edit_settings_action.setStatusTip("Press this to Edit Query Settings")
+        edit_settings_action.setStatusTip("Edit Query Settings or Set API key")
         edit_settings_action.triggered.connect(self.edit_settings_action_triggered)
         settings_menu.addAction(edit_settings_action)
 
-        # settings_menu.addAction("Ada")
-        # settings_menu.addAction("Babbage")
-        # settings_menu.addAction("Curie")
-        # settings_menu.addAction("Da Vinci")
+        # Help Menu
+        help_menu = self.addMenu("Help")
+
+        # Reference Manual Action
+        reference_manual_action = QAction("Reference Manual", self)
+        reference_manual_action.setStatusTip("Information about the Reference Manual")
+        reference_manual_action.triggered.connect(self.reference_manual_action_triggered)
+        help_menu.addAction(reference_manual_action)
+
+        # About Application Action
+        about_application_action = QAction("About", self)
+        about_application_action.setStatusTip("Information about the Application")
+        about_application_action.triggered.connect(self.about_application_action_triggered)
+        help_menu.addAction(about_application_action)
+
+        # About Pricing Action
+        about_pricing_action = QAction("Pricing Details", self)
+        about_pricing_action.setStatusTip("Information about the Pricing")
+        about_pricing_action.triggered.connect(self.about_pricing_action_triggered)
+        help_menu.addAction(about_pricing_action)
 
         # Other Menus
-        self.addMenu("Window")
-        self.addMenu("Help")
+        # self.addMenu("Window")
+        # self.addMenu("Help")
 
+    def reference_manual_action_triggered(self):
+        print("Reference Manual Triggered")
 
+        message = QMessageBox()
+        message.setMinimumSize(300, 200)
+        message.setWindowTitle("Reference Manual")
+
+        f_data = ""
+        filename = "/home/darklord/Desktop/openai-client/reference_manual.txt"
+        # If File does not exist create and empty file
+        if not os.path.exists(filename):
+            open(filename, 'w').close()
+        # if the file is empty, put default settings in the file
+        if os.stat(filename).st_size == 0:
+            f_data = "Reference Manual is not Available"
+        else:  # The file contains information about the reference manual
+            with open(filename, "r") as f:
+                f_data = f.readline()
+
+        message.setText(f_data)
+        # message.setInformativeText("Do you want to do something about it")
+        message.setIcon(QMessageBox.Information)
+        message.setStandardButtons(QMessageBox.Ok)
+        message.setDefaultButton(QMessageBox.Ok)
+
+        # Show the message box
+        ret = message.exec()
+        if ret == QMessageBox.Ok:
+            print("User chose Ok")
+        else:
+            print("User chose unknown button")
+    def about_application_action_triggered(self):
+        print("About Application Triggered")
+
+        message = QMessageBox()
+        message.setMinimumSize(300, 200)
+        message.setWindowTitle("About OpenAI client")
+
+        f_data = ""
+        filename = "/home/darklord/Desktop/openai-client/about_application.txt"
+        # If File does not exist create and empty file
+        if not os.path.exists(filename):
+            open(filename, 'w').close()
+        # if the file is empty, put default settings in the file
+        if os.stat(filename).st_size == 0:
+            f_data = "About Application Information is not Available"
+        else:  # The file contain information about application
+            with open(filename, "r") as f:
+                f_data = f.readline()
+
+        message.setText(f_data)
+        # message.setInformativeText("Do you want to do something about it")
+        message.setIcon(QMessageBox.Information)
+        message.setStandardButtons(QMessageBox.Ok)
+        message.setDefaultButton(QMessageBox.Ok)
+
+        # Show the message box
+        ret = message.exec()
+        if ret == QMessageBox.Ok:
+            print("User chose Ok")
+        else:
+            print("User chose unknown button")
+
+    def about_pricing_action_triggered(self):
+        print("About Pricing")
+
+        message = QMessageBox()
+        message.setMinimumSize(300, 200)
+        message.setWindowTitle("Pricing Details")
+
+        pricing_data = ""
+        filename = "/home/darklord/Desktop/openai-client/pricing_details.txt"
+        # If File does not exist create and empty file
+        if not os.path.exists(filename):
+            open(filename, 'w').close()
+        # if the file is empty, put default settings in the file
+        if os.stat(filename).st_size == 0:
+            pricing_data = "Pricing Information is not Available"
+        else:# The file contains the pricing details
+            with open(filename, "r") as f:
+                pricing_data = f.readline()
+
+        message.setText(pricing_data)
+        # message.setInformativeText("Do you want to do something about it")
+        message.setIcon(QMessageBox.Information)
+        message.setStandardButtons(QMessageBox.Ok)
+        message.setDefaultButton(QMessageBox.Ok)
+
+        # Show the message box
+        ret = message.exec()
+        if ret == QMessageBox.Ok:
+            print("User chose Ok")
+        else:
+            print("User chose unknown button")
+
+    def edit_settings_action_triggered(self):
+        print("Edit Query Settings")
+        dialog = EditQuerySettingsDialog()
+        dialog.exec()
 
     def edit_settings_action_triggered(self):
         print("Edit Query Settings")
@@ -88,11 +204,12 @@ class MenuBarMain(QMenuBar):
         dialog.exec()
 
 
+
     # History Actions
     def show_history_action_clicked(self):
-        print("Button-Hard clicked")
+        print("Show History Action clicked")
         message = QMessageBox()
-        message.setMinimumSize(700,300)
+        message.setMinimumSize(700, 300)
         message.setWindowTitle("Chat History")
 
         # storage = StorageHistory()
@@ -104,18 +221,15 @@ class MenuBarMain(QMenuBar):
         message.setText(f"This is the chat history ")
         message.setInformativeText("Do you want to do something about it")
         # message.setIcon(QMessageBox.Critical)
-        message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        message.setStandardButtons(QMessageBox.Ok)
         message.setDefaultButton(QMessageBox.Ok)
 
         # Show the message box
         ret = message.exec()
-        if ret == QMessageBox.Ok :
+        if ret == QMessageBox.Ok:
             print("User chose Ok")
-        elif ret == QMessageBox.Cancel :
-            print("User chose Cancel")
         else:
             print("User chose unknown button")
-
 
     def clear_history_action_clicked(self):
         print("Clear History Action clicked")
@@ -144,6 +258,3 @@ class MenuBarMain(QMenuBar):
             print("User chose Cancel")
         else:
             print("User chose unknown button")
-
-
-
