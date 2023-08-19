ec6aab7e9cda9b588f5c2d85732ea38f0e2eb75c
Anirban Dey <code@soufrabi.com>
2023-08-19 15:03:56 +0530

    check path new script

--- scripts/windows-install.ps1
+++ scripts/windows-install.ps1
@@ -1,5 +1,6 @@
 
 
+
 # Function to check if a directory exists
 function Test-DirectoryExists {
     param (
@@ -13,6 +14,21 @@ function Test-DirectoryExists {
     }
 }
 
+function Create-FolderIfNotExists {
+    param (
+        [string]$FolderPath
+    )
+
+    # Check if the folder already exists
+    if (-not (Test-Path -Path $FolderPath -PathType Container)) {
+        # Create the folder
+        New-Item -Path $FolderPath -ItemType Directory | Out-Null
+        Write-Host "Folder '$FolderPath' created."
+    } else {
+        Write-Host "Folder '$FolderPath' already exists."
+    }
+}
+
 # Function to check if the script is running with administrative privileges
 function Test-AdminPrivileges {
     $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
@@ -23,20 +39,35 @@ function Test-AdminPrivileges {
 }
 
 # Function to add a directory to the system-wide PATH
-function Add-DirectoryToPath {
-    param (
-        [string]$DirectoryPath
-    )
+# function Add-DirectoryToPath {
+#     param (
+#         [string]$DirectoryPath
+#     )
     
-    # Check if the directory is already in the PATH
-    $existingPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+#     # Check if the directory is already in the PATH
+#     $existingPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
     
-    if ($existingPath -notlike "*$DirectoryPath*") {
-        # Add the directory to the PATH
-        [System.Environment]::SetEnvironmentVariable("Path", "$existingPath;$DirectoryPath", [System.EnvironmentVariableTarget]::Machine)
-        Write-Host "Added $DirectoryPath to the system-wide PATH."
-    } else {
-        Write-Host "$DirectoryPath is already in the system-wide PATH."
+#     if ($existingPath -notlike "*$DirectoryPath*") {
+#         # Add the directory to the PATH
+#         [System.Environment]::SetEnvironmentVariable("Path", "$existingPath;$DirectoryPath", [System.EnvironmentVariableTarget]::Machine)
+#         Write-Host "Added $DirectoryPath to the system-wide PATH."
+#     } else {
+#         Write-Host "$DirectoryPath is already in the system-wide PATH."
+#     }
+# }
+
+function AddTo-Path{
+param(
+    [string]$Dir
+)
+
+    if( !(Test-Path $Dir) ){
+        Write-warning "Supplied directory was not found!"
+        return
+    }
+    $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+    if( $PATH -notlike "*"+$Dir+"*" ){
+        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "Machine")
     }
 }
 
@@ -52,21 +83,18 @@ $Dest = "openai-client.exe"
 # Check for administrative privileges
 Test-AdminPrivileges
 
+# Create AppDirectory
+Create-FolderIfNotExists -FolderPath $AppDirectory
+
+Set-Location $AppDirectory
+
 # Download
 Invoke-WebRequest -Uri  $Uri -OutFile $Dest
 
 
-# Check if the directory already exists
-if (-not (Test-Path -Path $DirectoryPath -PathType Container)) {
-    # Create the directory
-    New-Item -Path $DirectoryPath -ItemType Directory
-    Write-Host "Created directory: $DirectoryPath"
-} else {
-    Write-Host "Directory $DirectoryPath already exists."
-}
 
 # Add the directory to the system-wide PATH
-Add-DirectoryToPath -DirectoryPath $AppDirectory
+AddTo-Path -Dir $AppDirectory
 
 $SourceFile = $Dest
 $DestinationDirectory = $AppDirectory
